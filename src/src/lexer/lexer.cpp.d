dd6786f0f17961fcebf0e62cc1cf32fc0e65d594
Salman <61201330+untamedImpala@users.noreply.github.com>
2021-11-13 14:52:08 +0500

    Delete src/src directory

--- src/src/lexer/lexer.cpp.d
+++
@@ -1,8 +0,0 @@
-src/./src/lexer/lexer.cpp.o: src/lexer/lexer.cpp src/lexer/./lexer.h \
- src/lexer/./../utils/pair.h src/lexer/./../utils/./token.h
-
-src/lexer/./lexer.h:
-
-src/lexer/./../utils/pair.h:
-
-src/lexer/./../utils/./token.h:
